eb08ee40c0dce244110b60ec14c3cc2f198baa45
rohit163 <rohitphatarpekar@gmail.com>
2020-12-02 15:49:26 +0530

    first

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 'use strict';
 
 // prettier-ignore
-const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
 const form = document.querySelector('.form');
 
