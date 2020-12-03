ff6a34c8c826e3124fa0b3ba6f9b475dec43a186
rohit163 <rohitphatarpekar@gmail.com>
2020-12-03 18:53:12 +0530

    maps

--- script.js
+++ script.js
@@ -7,7 +7,22 @@ const form = document.querySelector('.form');
 if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(
     function (position) {
-      console.log(position);
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      console.log(latitude, longitude);
+      const coords = [latitude, longitude];
+      //code copied form leaflet js site visit it to get more info
+      const map = L.map('map').setView(coords, 15);
+
+      L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(coords)
+        .addTo(map)
+        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        .openPopup();
     },
     function () {
       alert('Could not get your position');
