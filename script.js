650c175d90dbcbe6c984e05180e611d01482122f
rohit163 <rohitphatarpekar@gmail.com>
2020-12-08 15:41:39 +0530

    overwrite

--- script.js
+++ script.js
@@ -6,11 +6,12 @@ const form = document.querySelector('.form');
 
 if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(
-    function (position) {
+    position => {
       const { latitude } = position.coords;
       const { longitude } = position.coords;
       console.log(latitude, longitude);
       const coords = [latitude, longitude];
+
       //code copied form leaflet js site visit it to get more info
       const map = L.map('map').setView(coords, 15);
 
@@ -19,10 +20,23 @@ if (navigator.geolocation) {
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      L.marker(coords)
-        .addTo(map)
-        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-        .openPopup();
+      map.on('click', mapEvent => {
+        console.log(mapEvent);
+        const { lat, lng } = mapEvent.latlng;
+        L.marker([lat, lng])
+          .addTo(map)
+          .bindPopup(
+            L.popup({
+              maxWidth: 250,
+              minWidth: 100,
+              autoClose: false,
+              closeOnClick: false,
+              className: 'style-popup',
+            })
+          )
+          .setPopupContent('workout')
+          .openPopup();
+      });
     },
     function () {
       alert('Could not get your position');
